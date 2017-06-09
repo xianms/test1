d544fd2e81aa6dc6ab82efb0625faade8bc3b463
xianms <xianms@xianmsdeMBP.lan>
2017-06-09 18:01:56 +0800

    update enable script

--- enable-vpn.sh
+++ enable-vpn.sh
@@ -1,9 +1,12 @@
 echo "1" > /proc/sys/net/ipv4/ip_forward
+apt-get update
 apt-get install -y bird
 apt-get install -y strongswan
 systemctl enable bird.service
 git clone https://github.com/xianms/test1.git
 cp test1/ipsec* /etc/
+cp test1/bird* /etc/bird/
 mkdir -p /run/bird/
 systemctl restart bird
 ipsec restart
+ip route add 10.10.10.0/24 via 10.171.242.201
